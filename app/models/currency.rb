99e017af66908ea7405b41339f84fe980262a06d
koomekevo <koome.kevo@gurucrafts.net>
2022-11-20 20:15:52 +0300

    Fix syntax error in current_price method

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,5 +1,5 @@
 class Currency < ApplicationRecord
-    def current_price:
+    def current_price
         url = "https://api.coinmarketcap.com/v1/ticker"
         request = HTTParty.get(url + self.slug)
         response = JSON.parse(request.body)
