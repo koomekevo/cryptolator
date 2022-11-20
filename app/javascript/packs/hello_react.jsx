3c41468177bba98cfe4c218efefc99539b230e28
koomekevo <koome.kevo@gurucrafts.net>
2022-11-20 17:34:17 +0300

    Install prop-types and ESLint

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -1,22 +1,28 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-const Hello = props => (
-    <div> Hello, {props.name}</div>
+const Hello = (props) => (
+  <div>
+    {' '}
+    Hello,
+    {' '}
+    {props.name}
+  </div>
 );
 
 Hello.defaultProps = {
-    name: 'David'
-}
+  name: 'David',
+};
 
-Hello.propTypes = { 
-    name: PropTypes.string
-}
+Hello.propTypes = {
+  name: PropTypes.string,
+};
 
 document.addEventListener('DOMContentLoaded', () => {
-   ReactDOM.render(
-    <Hello name="React" />, 
+  ReactDOM.render(
+    <Hello name="React" />,
     document.body.appendChild(document.createElement('div')),
-   )
-})
+  );
+});
