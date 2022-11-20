29a719f73acc1b844ca707132cdc95aadff44fe3
koomekevo <koome.kevo@gurucrafts.net>
2022-11-20 16:25:17 +0300

    Add currencies#index, search and calculate to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "currencies#index"
+  post "search", to: "currencies#search"
+  post "calculate", to: "currencies#calculate"
 end
